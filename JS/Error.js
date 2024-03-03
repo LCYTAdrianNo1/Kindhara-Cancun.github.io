93adc6c2d9cdd4b0a5aeb157d4722eba770d409d
LCYTAdrianNo1 <80604147+LCYTAdrianNo1@users.noreply.github.com>
2024-03-02 20:40:30 -0500

    V1.0.020324
    -Se añadio que los errores que escriban en consola

---
+++ JS/Error.js
@@ -0,0 +1,7 @@
+try {
+    // Código que puede generar un error
+    throw new Error('Este es un error de ejemplo.');
+} catch (error) {
+    // Registra el error en la consola
+    console.error('Se produjo un error:', error);
+}
