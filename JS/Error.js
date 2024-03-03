6bd3280563bb294d5246ec7470d7aeb427be6395
LCYTAdrianNo1 <80604147+LCYTAdrianNo1@users.noreply.github.com>
2024-03-02 21:40:58 -0500

    V1.1.020324
    -Se elimino el ejemplo del error en consola

--- JS/Error.js
+++ JS/Error.js
@@ -1,6 +1,4 @@
 try {
-    // Código que puede generar un error
-    throw new Error('Este es un error de ejemplo.');
 } catch (error) {
     // Registra el error en la consola
     console.error('Se produjo un error:', error);
